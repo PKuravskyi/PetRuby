4f7222cf0418ec3d0d63c07b2b7f8f92750a61af
Pavlo Kuravskyi <pavlo.kuravskyi@teladochealth.com>
2023-05-14 21:39:14 +0300

    Update caesar_cipher.rb

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -2,15 +2,15 @@
 
 class CaesarCipher
   def caesar_cipher(str, shift)
-    alphabet = ('a'..'z').to_a # create an array of all lowercase letters
-    new_str = '' # create an empty string to store the encrypted message
+    alphabet = ('a'..'z').to_a
+    new_str = ''
 
     str.each_char do |char|
       if char.match(/[a-zA-Z]/)
-        old_idx = alphabet.index(char.downcase) # find the index of the current character in the alphabet
-        new_idx = (old_idx + shift) % 26 # shift the index by the given amount (wrapping around if necessary)
-        new_char = alphabet[new_idx] # find the new character corresponding to the new index
-        new_str += char == char.upcase ? new_char.upcase : new_char.downcase # preserve the case of the original character
+        old_idx = alphabet.index(char.downcase)
+        new_idx = (old_idx + shift) % 26
+        new_char = alphabet[new_idx]
+        new_str += char == char.upcase ? new_char.upcase : new_char.downcase
       else
         new_str += char
       end
